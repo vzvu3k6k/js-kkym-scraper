3bd6855952b03a6e9fcfb0666e557946c64e1dfb
vzvu3k6k <vzvu3k6k@gmail.com>
2016-03-12 02:42:36 +0900

    Update test/widget-works-scraper.js
    Use real HTML as fixtures instead of hand-built dummy HTML

--- test/widget-works-scraper.js
+++ test/widget-works-scraper.js
@@ -1,69 +1,77 @@
 import assert from 'assert'
 import fs from 'fs'
 import path from 'path'
-import Scraper from '../src/recent-works-scraper'
+import Scraper from '../src/widget-works-scraper'
 
+// wget https://kakuyomu.jp/users/pureone/works --clobber -O ./test/fixtures/widgetWorks/mini.html
 {
-  const html = fs.readFileSync(path.resolve(__dirname, './fixtures/mini.html'))
+  const html = fs.readFileSync(path.resolve(__dirname, './fixtures/widgetWorks/mini.html'), 'utf-8')
   const scraper = new Scraper(html)
-  assert.deepEqual([...scraper.getItems()], [{
-    name: '我輩は猫である',
-    workId: '1',
+  const work = [...scraper.getItems()].pop()
+
+  assert.deepEqual(work, {
+    name: 'テスト',
+    workId: '4852201425154936607',
     author: {
-      userId: 'natsumesouseki',
-      name: '夏目漱石'
+      userId: 'pureone',
+      name: '@pureone'
     },
     reviewPoints: 0,
-    genre: 'ファンタジー',
+    genre: 'その他',
     status: '連載中',
     episodeCount: 1,
-    characterCount: 100,
-    dateModified: new Date(1905, 9, 6, 6, 0),
+    characterCount: 41,
+    dateModified: new Date(2016, 1 - 1, 31, 18, 17),
     introductionSnippet: null,
     flags: [],
     keywords: [],
-    firstEpisodeId: '2',
     reviews: [],
-    imageColor: null
-  }])
+    imageColor: '#000000'
+  })
 }
 
+// wget https://kakuyomu.jp/users/kawango/works --clobber -O ./test/fixtures/widgetWorks/full.html
 {
-  const html = fs.readFileSync(path.resolve(__dirname, './fixtures/full.html'))
+  const html = fs.readFileSync(path.resolve(__dirname, './fixtures/widgetWorks/full.html'), 'utf-8')
   const scraper = new Scraper(html)
-  assert.deepEqual([...scraper.getItems()], [{
-    name: '羅生門',
-    workId: '1234567890123456789',
+  const work = [...scraper.getItems()].pop()
+
+  assert(work.reviewPoints > 0)
+  delete work.reviewPoints
+
+  assert(work.name.length > 0)
+  delete work.name
+
+  assert(work.episodeCount >= 3)
+  delete work.episodeCount
+
+  assert(work.characterCount >= 4322)
+  delete work.characterCount
+
+  assert(work.dateModified >= new Date(2016, 3 - 1, 7, 8, 1))
+  delete work.dateModified
+
+  assert(work.introductionSnippet.length > 0)
+  delete work.introductionSnippet
+
+  assert.strictEqual(work.reviews.length, 2)
+  assert.deepStrictEqual(work.reviews[0].author, {
+    userId: 'kawango',
+    name: 'かわんご'
+  })
+  assert(work.reviews[0].body.length > 0)
+  delete work.reviews
+
+  assert.deepStrictEqual(work, {
+    workId: '1177354054880357352',
     author: {
-      userId: 'akutagawa-ryunosuke',
-      name: '芥川龍之介'
+      userId: 'kawango',
+      name: 'かわんご'
     },
-    reviewPoints: 1234,
-    genre: '歴史・時代',
-    status: '完結済',
-    episodeCount: 1,
-    characterCount: 12345,
-    dateModified: new Date(2015, 11, 25, 10, 15),
-    introductionSnippet: 'ある日の暮方の事である。\n一人の下人が、羅生門の下で雨やみを待っていた。',
-    flags: ['残酷描写有り', '暴力描写有り'],
-    keywords: ['ホラー', '短編'],
-    firstEpisodeId: '9876543210987654321',
-    reviews: [
-      {
-        author: {
-          userId: 'akutagawa-ryunosuke',
-          name: '芥川龍之介'
-        },
-        body: '自分はそれだけで満足であった。'
-      },
-      {
-        author: {
-          userId: 'ge_nin',
-          name: '下人'
-        },
-        body: '何をしていた。云え。云わぬと、これだぞよ。'
-      }
-    ],
-    imageColor: '#0033C4'
-  }])
+    genre: 'ホラー',
+    status: '連載中',
+    flags: ['残酷描写有り', '暴力描写有り', '性描写有り'],
+    keywords: ['ハートフルシリーズ', 'ピアピア動画', 'ＳＦ'],
+    imageColor: '#FF9D00'
+  })
 }
